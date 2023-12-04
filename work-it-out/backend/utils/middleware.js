0d0f4590adbdf1e84770c81627a30933fe37867e
Joseph <zhao.joseph@yahoo.ca>
2023-12-04 16:43:03 -0500

    Updated cookie to secure

--- work-it-out/backend/utils/middleware.js
+++ work-it-out/backend/utils/middleware.js
@@ -1,4 +1,7 @@
 const isAuthenticated  = async (req, res, next) => {
+
+    console.log(req.session);
+
     if(!req.session.user){
         return res.status(401).send({Message : "Unauthorized"})
     };
